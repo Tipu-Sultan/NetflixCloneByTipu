998562ba06163ff260efd5105684370d58a16049
Tipu Sultan <63140791+Tipu-Sultan@users.noreply.github.com>
2023-08-09 15:39:19 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate, Link,useLocation } from "react-router-dom";
 
 const Header = () => {
   const navigate = useNavigate();
-
+  const location = useLocation();
+  const page = location.pathname === '/dashboard' ? true:false;
   const clickHandler = (e) => {
-    e.preventDefault();
+    e.preventDefault() ;
     navigate('/login');
   }
-
+  const WeatherclickHandler = (e) => {
+    e.preventDefault() ;
+    navigate('/weather');
+  }
+  const getlang = (e) => {
+    e.preventDefault() ;
+    alert('language not available');
+  }
   return(
     <header className="topNav">
       <nav className="navbar navbar-expand-md navbar-dark">
@@ -16,14 +24,17 @@ const Header = () => {
             <img className="nav__logo" src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" alt="" />
           </Link>
           
-          <div className="navbar">
-            <form className="d-flex" role="search">
-              <select>
+          <div className="navbar-nav">
+          <Link className="nav-item nav-link active" to="/movies">
+            <p>Movies</p>
+          </Link>
+            <form className="d-flex " role="search">
+              <select onChange={getlang}>
                 <option>English</option>
                 <option>Hindi</option>
               </select>
-              <button className="btn btn-danger" onClick={clickHandler}>Signin</button>
             </form>
+            <button className="btn btn-danger mx-4" onClick={clickHandler}>{ page ? 'Logout' : 'Sign In'}</button>
           </div>
         </div>
       </nav>
