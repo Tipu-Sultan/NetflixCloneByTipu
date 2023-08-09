9285f585042402cd5d0dc9405691bb9e3c1b4ebe
Tipu Sultan <tipu@student.iul.ac.in>
2023-08-09 15:17:09 +0530

    Initial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyAI5pb9Kv7_MSWGa5WkhiWCjvM2EbeNSZM",
+  authDomain: "tubeline-4f769.firebaseapp.com",
+  projectId: "tubeline-4f769",
+  storageBucket: "tubeline-4f769.appspot.com",
+  messagingSenderId: "1019309939671",
+  appId: "1:1019309939671:web:54ba8f750b27d056b34992",
+  measurementId: "G-K52QL6NSCF"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
